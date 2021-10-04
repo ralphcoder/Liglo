7ca40b486fe54302d167f40c27f6aa2abae9bd76
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2021-10-04 23:02:28 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const KGoldFontColor = Color(0XFFdaceb7);
+const KGoldFontColor = Color(0XFFdaceb7);//basic theme of app
 const Klightbrowntextcolor = Color(0XFFb08475);
 const Kbrowniconcolor = Color(0XFFfe9870);
 const Kdakbrowntextcolor = Color(0XFF4f2e2d);
